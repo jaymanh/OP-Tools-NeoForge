f7ba082f787cd409cb48cbed0459bd1d98d7f5f0
jaymanh <65929600+jaymanh@users.noreply.github.com>
2026-08-27 08:53:00 +1000

    Fix ice obtainability, diamondium armor repair, refinery persist/hoppers/drops, leftover replant datapack, shovels.

--- src/main/java/jaymanh/optools/Armor/ModArmorMaterials.java
+++ src/main/java/jaymanh/optools/Armor/ModArmorMaterials.java
@@ -1,10 +1,10 @@
 package jaymanh.optools.Armor;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.TagKey;
-import net.minecraft.Util;
+import net.minecraft.util.Util;
 import net.minecraft.world.item.equipment.ArmorMaterial;
 import net.minecraft.world.item.equipment.ArmorMaterials;
 import net.minecraft.world.item.equipment.ArmorType;
@@ -12,6 +12,7 @@ import java.util.EnumMap;
 
 import static jaymanh.optools.OpTools.MOD_ID;
 import static jaymanh.optools.Tools.TagKeys.ItemKeys.DIAMONDILLIUM_REPAIR_ITEMS;
+import static jaymanh.optools.Tools.TagKeys.ItemKeys.DIAMONDIUM_REPAIR_ITEMS;
 
 public class ModArmorMaterials implements ArmorMaterials {
 
@@ -24,14 +25,14 @@ public class ModArmorMaterials implements ArmorMaterials {
         map.put(ArmorType.LEGGINGS, 9);
         map.put(ArmorType.CHESTPLATE, 7);
         map.put(ArmorType.HELMET, 4);
-    }), 24, SoundEvents.ARMOR_EQUIP_NETHERITE, 4, 1, TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID,"diamondillium_repair_item")), CustomArmorModels.DIAMONDILLIUM);
+    }), 24, SoundEvents.ARMOR_EQUIP_NETHERITE, 4, 1, TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID,"diamondillium_repair_item")), CustomArmorModels.DIAMONDILLIUM);
 
     public static final ArmorMaterial DIAMONDIUM = new ArmorMaterial(DIAMONDIUM_DURABILITY_MULTIPLIER, Util.make(new EnumMap(ArmorType.class), map -> {
         map.put(ArmorType.BOOTS, 7);
         map.put(ArmorType.LEGGINGS, 15);
         map.put(ArmorType.CHESTPLATE, 11);
         map.put(ArmorType.HELMET, 6);
-    }), 24, SoundEvents.ARMOR_EQUIP_NETHERITE, 6, 1.5f, DIAMONDILLIUM_REPAIR_ITEMS, CustomArmorModels.DIAMONDIUM);
+    }), 24, SoundEvents.ARMOR_EQUIP_NETHERITE, 6, 1.5f, DIAMONDIUM_REPAIR_ITEMS, CustomArmorModels.DIAMONDIUM);
 
     public static void initialise(){
 
